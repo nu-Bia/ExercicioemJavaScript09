a04497f8f2dbadf359df7f6053420c41eee2fd05
Núbia <112019351+nu-Bia@users.noreply.github.com>
2022-10-05 15:18:50 -0300

    Add files via upload
    9) Escreva um algoritmo para ler as dimensões de um quadrado (lado), calcular e escrever a área do quadrado. Dica a=l²

---
+++ quadrado.js
@@ -0,0 +1,5 @@
+function multiplique(){
+    var numero1=prompt(("digite o valor do lado do quadrdao:"));
+    var multiplique=numero1*numero1
+    alert("A área do quadrado é :"+multiplique);
+    }
